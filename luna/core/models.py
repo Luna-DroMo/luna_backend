6e105310ec06798f32dafa4e7be59e6bbea6a7cc
SGeetansh <saxenageetansh1@gmail.com>
2023-09-26 10:48:03 +0200

    User and student model implemented. On signup, a student gets created.

--- luna/core/models.py
+++ luna/core/models.py
@@ -1,18 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
-# Create your CustomUserManager here.
+"""
+Django by default supports login via username and password. To implement the functionality to login via email,
+we need to create a custom user.
+For more information, google this thing. Also study the difference between AbstractUser and AbstractBaseUser.
+"""
+
+
 class CustomUserManager(BaseUserManager):
     def _create_user(self, email, password, first_name, last_name, **extra_fields):
         if not email:
             raise ValueError("Email must be provided")
         if not password:
-            raise ValueError('Password is not provided')
+            raise ValueError("Password is not provided")
 
         user = self.model(
-            email = self.normalize_email(email),
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
             **extra_fields
         )
 
@@ -21,34 +31,47 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password, first_name, last_name, **extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_active',True)
-        extra_fields.setdefault('is_superuser',False)
-        return self._create_user(email, password, first_name, last_name, password, **extra_fields)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(
+            email, password, first_name, last_name, password, **extra_fields
+        )
 
     def create_superuser(self, email, password, first_name, last_name, **extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_active',True)
-        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", True)
         return self._create_user(email, password, first_name, last_name, **extra_fields)
 
-# Create your User Model here.
-class User(AbstractBaseUser,PermissionsMixin):
-    # Abstractbaseuser has password, last_login, is_active by default
 
+# Create your User Model here.
+class User(AbstractBaseUser, PermissionsMixin):
+    # Abstractbaseuser has password, last_login, is_active by default
     email = models.EmailField(db_index=True, unique=True, max_length=254)
     first_name = models.CharField(max_length=240)
     last_name = models.CharField(max_length=255)
 
-    is_staff = models.BooleanField(default=True) # must needed, otherwise you won't be able to loginto django-admin.
-    is_active = models.BooleanField(default=True) # must needed, otherwise you won't be able to loginto django-admin.
-    is_superuser = models.BooleanField(default=False) # this field we inherit from PermissionsMixin.
+    is_staff = models.BooleanField(default=True)
+    # must needed, otherwise you won't be able to loginto django-admin.
+    is_active = models.BooleanField(default=True)
+    # must needed, otherwise you won't be able to loginto django-admin.
+    is_superuser = models.BooleanField(default=False)
+    # this field we inherit from PermissionsMixin.
+
+    class UserType(models.IntegerChoices):
+        STUDENT = 1
+        LECTURER = 2
+        ADMIN = 3
+        OTHER = 4  # Keeping this for any future needs
+
+    user_type = models.IntegerField(choices=UserType.choices)
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name','last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = "User"
+        verbose_name_plural = "Users"
--- luna/core/views.py
+++ luna/core/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.authentication import (
+    SessionAuthentication,
+    TokenAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from .models import User
+
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+@api_view(["POST"])
+def login(request):
+    user = get_object_or_404(User, email=request.data["email"])
+    if not user.check_password(request.data["password"]):
+        return Response(
+            {"detail": "Not found."},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token": token.key, "user": serializer.data})
+    # return Response({})
+
+
+@api_view(["POST"])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(email=request.data["email"])
+        user.set_password(request.data["password"])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST,
+    )
+    # return Response({})
+
+
+@api_view(["GET"])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response("passed!")
+    # return Response({})
