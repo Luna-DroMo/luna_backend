e10ae6687b650ef52909a818eeb68cd5794958d5
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-01-02 12:38:26 +0100

    change env structure according to azure

--- luna/luna/settings.py
+++ luna/luna/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = "luna.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DATABASE_NAME"),
-        "USER": config("DATABASE_USER"),
-        "PASSWORD": config("DATABASE_PASSWORD"),
-        "HOST": config("DATABASE_HOST"),
-        "PORT": config("DATABASE_PORT"),
+        "NAME": config("PGNAME"),
+        "USER": config("PGUSER"),
+        "PASSWORD": config("PGPASSWORD"),
+        "HOST": config("PGHOST"),
+        "PORT": config("PGPORT"),
     }
 }
 
