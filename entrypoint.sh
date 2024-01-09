04acf6991a4ebeb9e3062767b0872ffad9a893a0
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-01-10 00:22:55 +0100

    change port to 80 as it is azure defaul

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
 
 # Wait for the database to be ready
 echo "Waiting for database."
 
 
-# Apply database migrations
-echo "Applying database migrations."
+# Wait for the database to be ready
+./wait-for-it.sh db:5432 -t 60
+
+# Run database migrations
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-# Start the Django development server
-echo "Starting Django server."
-python3 manage.py runserver 0.0.0.0:8000
+# Start the Django server
+python3 manage.py runserver 0.0.0.0:80
 
