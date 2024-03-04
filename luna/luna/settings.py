cb3f9672527357ab26657cece3010ac8ac946ed6
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-03-04 15:38:34 +0300

    add port

--- luna/luna/settings.py
+++ luna/luna/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'USER': os.getenv('PGUSER'),
         'PASSWORD': os.getenv('PGPASSWORD'),
         'HOST': os.getenv('PGHOST'),
-        'PORT': os.getenv('PGHOST')
+        'PORT': os.getenv('PGPORT')
     }
 }
 
